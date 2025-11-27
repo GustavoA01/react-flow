a572ff6f8d6fd20d3894d124d4e7382dafe3f295
GustavoA01 <gustavoaguiar14325@gmail.com>
2025-11-27 17:18:19 -0300

    correcoes e vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      allowedHosts: env.VITE_ALLOW_ALL_HOSTS === 'true' ? true : [],
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
 })
