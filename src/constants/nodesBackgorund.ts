ab522712c48f145b9eb07c80e19d5f2aac73c338
GustavoA01 <gustavoaguiar14325@gmail.com>
2025-11-28 18:37:45 -0300

    imagem de fundo 1

--- src/constants/nodesBackgorund.ts
+++ src/constants/nodesBackgorund.ts
@@ -1,4 +1,5 @@
-import img1 from '../assets/map-background/1_estacao.png';
+import img1Puc from '../assets/map-background/fundo1-puc.svg';
+import img1SaoGabriel from '../assets/map-background/fundo1-estacao-sao-gabriel.svg';
 
 export const backgroundNodes = [
   {
@@ -7,8 +8,22 @@ export const backgroundNodes = [
     position: { x: -1920, y: -1800 }, 
     data: { 
       id:'1',
-      image: img1, 
+      image: img1Puc, 
       width: 2000, 
+      height: 2800 
+    },
+    zIndex: -10,
+    draggable: false,
+    selectable: false,
+  },
+  {
+    id: 'bg-img1-estacao-sao-gabriel',
+    type: 'background',
+    position: { x: 78, y: -1800 }, 
+    data: { 
+      id:'1',
+      image: img1SaoGabriel, 
+      width: 1920, 
       height: 2800 
     },
     zIndex: -10,
